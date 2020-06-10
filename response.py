5051a6f8cdc1f88963c140fc11cfb226abd0e260
VasaviLagishetty <vasavi2910@gmail.com>
2020-06-10 21:22:19 +0530

    added same reply message

--- response.py
+++ response.py
@@ -6,4 +6,8 @@ bot = telebot.TeleBot(API_TOKEN)
 def send_welcome(message):
   bot.reply_to(message, "\nHi there, I am SampleDemoBot.\nI am here to repeat your words. Just send anything  and I'll send the same thing to you!\n")
   
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+  bot.reply_to(message, message.text)
+  
 bot.polling()
