b000cd2dcbfcedef0203d7c5570868326d911b1d
VasaviLagishetty <vasavi2910@gmail.com>
2020-06-10 21:19:34 +0530

    first commit

--- response.py
+++ response.py
@@ -5,5 +5,9 @@ bot = telebot.TeleBot(API_TOKEN)
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
   bot.reply_to(message, "\nHi there, I am SampleDemoBot.\nI am here to repeat your words. Just send anything  and I'll send the same thing to you!\n")
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+  bot.reply_to(message, message.text)
   
 bot.polling()
